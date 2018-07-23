a99c525ee1c9bafc4002404032fea1e0d1581584
Jagadeesh Rampam <jagadeeshwithu@gmail.com>
2018-07-23 19:51:14 +0530

    New Project: Using Reactive Forms

---
+++ ReactiveFormsExample/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ReactiveFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ReactiveFormsExample/src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+import { IUser } from './reactive-form.interface';
+
+
+@Component({
+  selector: 'reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent implements OnInit {
+
+  private user: IUser[];
+  private form: FormGroup;
+
+  handleSubmit(ev) {
+    // console.log(ev);
+    // console.log(form);
+    console.log(this.form.value);
+    
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+
+    this.form = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(4)]),
+      email: new FormControl('',[Validators.required, Validators.email] )
+    })
+  }
+
+}
