347165aa80d3fb5964971b035421b11d9a27ee9b
Jagadeesh Rampam <jagadeeshwithu@gmail.com>
2018-07-24 00:35:15 +0530

    Added the documentation as well for custom validators and Async Validators

--- ReactiveFormsExample/src/app/reactive-form/reactive-form.component.ts
+++ ReactiveFormsExample/src/app/reactive-form/reactive-form.component.ts
@@ -26,8 +26,56 @@ export class ReactiveFormComponent implements OnInit {
 
     this.form = new FormGroup({
       name: new FormControl('', [Validators.required, Validators.minLength(4)]),
-      email: new FormControl('',[Validators.required, Validators.email] )
-    })
+      // email: new FormControl('',[Validators.required, Validators.email] )
+      email: new FormControl(
+        '', 
+         [
+            Validators.required, 
+            Validators.email,
+            this.customValidator1ForEmail,
+            this.customValidator2ForEmail
+          ],
+          [
+            //Async Validators
+          ]
+      )
+    },
+    [this.customValidator1ForGroup]
+  )
   }
 
+  customValidator1ForEmail(fc: FormControl): any {
+    console.log(fc.value);
+    /* checking for Hot Mail Account */
+    // False --> valid, true --> invalid
+    return {
+      "HotMailAccount": false
+    };
+  }
+
+  customValidator2ForEmail(fc: FormControl): any {
+  
+    console.warn(fc.value);
+    return {
+      "NotAGmailAccount" : true 
+    };
+  }
+
+  customValidator1ForGroup(fg: FormGroup): any {
+    let n: string = fg.controls.name.value;
+    let e: string = fg.controls.email.value;
+    console.info(fg.controls.name.value, fg.controls.email.value);
+
+    if (n === "Jagadeesh" && e === 'jagadeeshwithu@gmail.com'){
+      return {
+        ComboOfNameAndEmail : true
+      }
+    }
+    else {
+      return {
+        ComboOfNameAndEmail : false
+      }
+    }
+    
+  }
 }
